0f62fb35dc38222f48d0809f6e0020d38169f78c
Abhinav Malviya <abhinavmalviya4321@gmail.com>
2025-08-21 01:39:27 +0530

    chang limk /

--- src/main/java/com/springProject/myFirstProject/myClass.java
+++ src/main/java/com/springProject/myFirstProject/myClass.java
@@ -1,12 +1,15 @@
 package com.springProject.myFirstProject;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class myClass {
-    @GetMapping("abc")
-    public String sayHello(){
-        return "chal raha hai";
+    @GetMapping("/")
+    public Map<String, String> sayHello(){
+        // Spring will automatically convert this Map to a JSON object
+        return Map.of("message", "chal raha hai", "status", "success");
     }
 
 }
